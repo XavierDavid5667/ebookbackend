122cbff625c4ff977e67959cf9ddf9ac40c4909d
xavierdavid5667 <xavierdavid5667@gmail.com>
2023-05-31 09:47:49 +0530

    Made some changes

--- thedailyscribbles/src/main/java/thedailyscribbles/exception/BloggerAlreadyExistsException.java
+++ thedailyscribbles/src/main/java/thedailyscribbles/exception/BloggerAlreadyExistsException.java
@@ -3,7 +3,7 @@ package thedailyscribbles.exception;
 public class BloggerAlreadyExistsException extends Exception {
 	private static final long serialVersionUID = 2L;
 
-	public BloggerAlreadyExistsException(String msg){
-		super(msg);
-	}
+//	public BloggerAlreadyExistsException(String msg){
+//		super(msg);
+//	}
 }
